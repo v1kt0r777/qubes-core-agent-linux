d9572a137ee549163b6d62f79a559908a7ad831a
Marek Marczykowski-Górecki <marmarek@invisiblethingslab.com>
2017-12-03 03:30:53 +0100

    network: IPv6-enabled firewall
    If IPv6 is configured in the VM, and it is providing network to others,
    apply IPv6 firewall similar to the IPv4 one (including NAT for outgoing
    traffix), instead of blocking everything. Also, enable IP forwarding for
    IPv6 in such a case.
    
    Fixes QubesOS/qubes-issues#718

--- vm-systemd/network-proxy-setup.sh
+++ vm-systemd/network-proxy-setup.sh
@@ -14,10 +14,15 @@ if [ "x$network" != "x" ]; then
     #netmask=$(qubesdb-read /qubes-netvm-netmask)
     primary_dns=$(qubesdb-read /qubes-netvm-primary-dns 2>/dev/null || echo "$gateway")
     secondary_dns=$(qubesdb-read /qubes-netvm-secondary-dns)
+    ip6=$(qubesdb-read /qubes-ip6 ||:)
     modprobe netbk 2> /dev/null || modprobe xen-netback || "${modprobe_fail_cmd}"
     echo "NS1=$primary_dns" > /var/run/qubes/qubes-ns
     echo "NS2=$secondary_dns" >> /var/run/qubes/qubes-ns
     /usr/lib/qubes/qubes-setup-dnat-to-ns
     echo "1" > /proc/sys/net/ipv4/ip_forward
+    # enable also IPv6 forwarding, if IPv6 is enabled
+    if [ -n "$ip6" ]; then
+        echo 1 > /proc/sys/net/ipv6/conf/all/forwarding
+    fi
     /sbin/ethtool -K eth0 sg off || true
 fi

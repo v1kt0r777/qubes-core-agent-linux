42a61d768440c4e6e496d3decda1e396c414e65a
Marek Marczykowski-Górecki <marmarek@invisiblethingslab.com>
2018-03-06 00:53:42 +0100

    Plug pylint into run-tests script
    Have it checked by travis

---
+++ test-packages/qubesdb.py
@@ -0,0 +1,11 @@
+class QubesDB():
+    def list(self, path):
+        return []
+    def read_watch(self):
+        return ''
+    def watch(self, path):
+        pass
+    def watch_fd(self):
+        return 0
+    def multiread(self, path_prefix):
+        return {}
